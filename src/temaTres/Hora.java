d4c1a23e3dfa454411cda549f8920e008542d501
likendero <likendero11@gmail.com>
2017-11-20 13:01:16 +0100

    clase hora metodos de restar

--- src/temaTres/Hora.java
+++ src/temaTres/Hora.java
@@ -26,7 +26,7 @@ public class Hora {
      */
     public Hora(){
         super();
-        this.hora=0;
+        this.hora=13;
         this.minutos=0;
         this.segundos=0;
     }
@@ -88,19 +88,19 @@ public class Hora {
         String fh="";
         String fm="";
         String fs="";
-        if(segundos>=9){
+        if(segundos>9){
             fs+=segundos;
         }else{
             fs+=0;
             fs+=segundos;
         }
-        if(minutos>=9){
+        if(minutos>9){
             fm+=minutos;
         }else{
             fm+=0;
             fm+=minutos;
         }
-        if(hora>=9){
+        if(hora>9){
             fh+=hora;
         }else{
             fh+=0;
@@ -149,5 +149,106 @@ public class Hora {
         if(hora==-1){
             hora=23;
         }
+        
     }
-}
+    /**
+     * esta clase calcula el tiempo restante hasta media noche
+     * @return tiempo restante formateado
+     */
+    public String tiempoHastaMediaNoche(){
+        int horilla,minutillos,segundillos;
+        horilla=0;
+        minutillos=0;
+        segundillos=0;
+        
+        if(this.getSegundos()!=0){
+           segundillos=60-this.getSegundos();
+        }
+        if(this.getMinutos()!=0){
+            minutillos=60-this.getMinutos();
+        }
+        if(this.getHora()!=0){
+            horilla=24-this.getHora();
+        }
+        return horilla+":"+minutillos+":"+segundillos;
+    }
+    /**
+     * resta dos horas devolviendo la diferencia con formato
+     * @param restador hora que se resta
+     * @return diferencia con formato
+     */
+    public String restarHoras(Hora restador){
+        int horilla,minutillos,segundillos;
+        horilla=this.getHora();
+        minutillos=this.getMinutos();
+        segundillos=this.getSegundos();
+        // segundo####################################################3
+        if(segundillos==0 && restador.getHora()>0){
+            minutillos-=1;
+            segundillos=60-restador.getSegundos();
+        }else if(segundillos<restador.getSegundos()){
+            segundillos-= restador.getSegundos();
+            segundillos+=60;
+            minutillos+=segundillos;
+        }else{
+            segundillos-= restador.getSegundos();
+        }
+        //minutos###################################################
+        //llevada de la anterior
+        if(minutillos<0){
+            minutillos+=60;
+            horilla-=1;
+            
+        }
+        //resta de los minutos
+        // si los minutos son 0 y hay minutos para restar
+        if(minutillos==0 && minutillos>0){
+            minutillos=60-restador.getMinutos();
+            horilla-=1;
+        //en el caso de que los minutos no sean 0 y se reste un numero mayor a estos
+        }else if(minutillos<restador.getMinutos()){
+            minutillos-=restador.getMinutos();
+            minutillos+=60;
+            horilla-=1;
+        // si no hay ningun problema en la resta
+        }else{
+            minutillos-=restador.getMinutos();
+        }
+        if(horilla<0){
+            horilla+=24;
+        }
+        //resta de las horas
+        //  si las horas son 0 y hay horas para restar
+        if(horilla==0 && restador.getHora()>0){
+            horilla=24-restador.getHora();
+        //en el caso de que la hora no sea 0 pero esta sea menor que la resta
+        }else if(horilla<restador.getHora()){
+            horilla-=restador.getHora();
+            horilla+=24;
+        // si no hay ningun problema en la resta
+        }else{
+            horilla-=restador.getHora();
+        }
+        String salida="";
+        if(horilla<10)salida+="0"+horilla+":";
+        else salida+=horilla+":";
+        if (minutillos<10)salida+="0"+minutillos+":";
+        else salida+= minutillos+":";
+        if (segundillos<10)salida+="0"+segundillos;
+        else salida+=segundillos;
+        
+        return salida; 
+        }
+    /**
+     * diferencia en segundos
+     * @param restador hora que se quiere restar
+     * @return valor entero qu erepresnta los segundos
+     */
+    public int restaVueltaSegundos(Hora restador){
+        // pasa todo a segundos y resta la diferencia devolviendo el valor absoluto
+        return Math.abs((this.getHora()*3600+this.getMinutos()*60+this.getSegundos())
+                - (restador.getHora()*3600+restador.getMinutos()*60+restador.getSegundos()));
+    }
+    }
+    
+
